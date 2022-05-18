41dc16af4ebcec9cb77404a0e3679f1b85620e1f
Ana-Maria <alcazaanamaria@gmail.com>
2022-05-18 23:21:44 +0300

    laboratorul 5

---
+++ src/com/company/StackClient.java
@@ -0,0 +1,68 @@
+package com.company;
+
+import java.io.*;
+import java.net.*;
+import  javax.sound.sampled.*;
+
+public class StackClient {
+
+    static AudioInputStream ais;
+    static AudioFormat format;
+    static int port = 50005;
+    static int sampleRate = 44100;
+
+    static DataLine.Info dataLineInfo;
+    static SourceDataLine sourceDataLine;
+
+    public static void main(String args[]) throws Exception {
+        System.out.println("Server started at port:" + port);
+        System.setProperty("java.net.preferIPv4Stack", "true");
+
+        try{
+            InetAddress group = InetAddress.getByName("225.6.7.8");
+            MulticastSocket mSocket = new MulticastSocket(port);
+            mSocket.setReuseAddress(true);
+            mSocket.joinGroup(group);
+//        DatagramSocket serverSocket = new DatagramSocket(port);
+
+            /*
+             * Formula for lag = (byte_size/sample_rate)*2
+             * Byte size 9728 will produce ~ 0.45 seconds of lag. Voice slightly broken.
+             * Byte size 1400 will produce ~ 0.06 seconds of lag. Voice extremely broken.
+             * Byte size 4000 will produce ~ 0.18 seconds of lag. Voice slightly more broken then 9728.
+             */
+
+            byte[] receiveData = new byte[4096];
+
+            format = new AudioFormat(sampleRate, 16, 2, true, false);
+            dataLineInfo = new DataLine.Info(SourceDataLine.class, format);
+            sourceDataLine = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
+            sourceDataLine.open(format);
+            sourceDataLine.start();
+
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            ByteArrayInputStream baiss = new ByteArrayInputStream(receivePacket.getData());
+
+            while (true)
+            {
+                mSocket.receive(receivePacket);
+                ais = new AudioInputStream(baiss, format, receivePacket.getLength());
+                toSpeaker(receivePacket.getData());
+            }
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void toSpeaker(byte soundbytes[]) {
+        try
+        {
+            System.out.println("At the speaker");
+            sourceDataLine.write(soundbytes, 0, soundbytes.length);
+        } catch (Exception e) {
+            System.out.println("Not working in speakers...");
+            e.printStackTrace();
+        }
+    }
+}
